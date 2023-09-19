033f2f847546637420fb9d2c1b23e85520aceeef
vella01 <136559282+vella01@users.noreply.github.com>
2023-09-19 14:18:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const http = require('http');
 const fs = require('fs');
 
+const Api = [
+    {
+        "name": "dheeraj",
+        "age": 19,
+        "vill": "bhalni"
+    },
+    {
+        "name": "rajneesh",
+        "age": 18,
+        "vill": "betauna"
+    },
+    {
+        "name": "nitish",
+        "age": 17,
+        "vill": "bhalni"
+    },
+    {
+        "name": "pawan",
+        "age": 18.2,
+        "vill": "bhalni"
+    }
+]
 let content = fs.readFileSync('text.txt', 'utf-8');
 
 //create a server
 
 let server = http.createServer((req, res)=>{
-    res.end(content + "Node js app deploying...");
+    res.end(Api);
 });
 
 server.listen(3000, ()=>{
